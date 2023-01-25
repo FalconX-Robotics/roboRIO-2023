4ec803deb4b1465504132b671f7a6a237b274517
FatCactis <githublogan@gmail.com>
2023-01-24 17:34:44 -0800

    fiexed an error and pid stuff

---
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -0,0 +1,31 @@
+// package frc.robot.commands;
+
+// import java.util.function.DoubleConsumer;
+// import java.util.function.DoubleSupplier;
+
+// import com.ctre.phoenix.sensors.PigeonIMU;
+
+// import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import edu.wpi.first.wpilibj2.command.PIDCommand;
+// import frc.robot.subsystems.Drivetrain;
+
+// public class AutoBalance extends PIDCommand {
+    
+//   private final PIDController m_controller;
+//   private DoubleSupplier m_measurement;
+//   private DoubleSupplier m_setpoint;
+//   private DoubleConsumer m_useOutput;
+
+//     Drivetrain m_drivetrain;
+//     PigeonIMU m_gyro;
+
+//     // public AutoBalance(Drivetrain drivetrain) {
+//     //     super(m_controller, m_setpoint, m_measurement, m_useOutput, null)
+//     //     // Will start auto for 3 seconds and then turn off
+//     //     m_drivetrain = drivetrain;
+//     //     m_gyro = drivetrain.getGyro();
+//     //     addRequirements(m_drivetrain);
+//     // }
+
+// }
