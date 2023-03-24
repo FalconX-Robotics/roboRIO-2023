9f72a9f293c4066324c1603f93ed49f2ea19443a
FatCactis <crawlingbeetles64@gmail.com>
2023-03-24 15:02:31 -0700

    Periodic Pressure Check
    Checks the pressure switch periodically

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -1,14 +1,18 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Pneumatics extends SubsystemBase {
 
     boolean clawOpen = false;
+    
+    private final Compressor m_compressor = new Compressor(20, PneumaticsModuleType.CTREPCM);
     
     private final DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid(20, PneumaticsModuleType.CTREPCM, 1, 0);
     public void open() {
@@ -18,6 +22,9 @@ public class Pneumatics extends SubsystemBase {
         m_doubleSolenoid.set(Value.kReverse);
     }
 
-    
+    @Override
+    public final void periodic() {
+        SmartDashboard.putBoolean("Pressure Switch On", m_compressor.getPressureSwitchValue());
+    }
 }
 
